e06753035906f38fe2d4fdf2574f7cb223815142
PAVEN NJADHAV <pavannjadhav12@gmail.com>
2025-04-11 21:37:46 +0530

    GetDone

--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -2,6 +2,8 @@ const express=require('express')
 const {
     registerUser,
     loginUser,
+    getAllUsers,
+    getUserById,
 }=require('../controllers/UserController')
 
 const authMiddleware=require('../Middleware/auth')
@@ -10,6 +12,8 @@ const router=express.Router()
 
 router.post("/register",registerUser);
 router.post("/login", loginUser);
+router.get("/allusers",authMiddleware,getAllUsers)
+router.get("/allusers/:id",authMiddleware,getUserById)
 
 
 
